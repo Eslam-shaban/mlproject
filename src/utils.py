0cdbe3728ef4ff2196b5d896b73644613273cc73
Eslam-shaban <73853163+Eslam-shaban@users.noreply.github.com>
2023-09-25 13:53:09 +0300

    Hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -23,7 +23,7 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train,X_test, y_test, models):
+def evaluate_models(X_train, y_train,X_test, y_test, models, params):
     try:
         report = {}
 
@@ -31,7 +31,15 @@ def evaluate_models(X_train, y_train,X_test, y_test, models):
             
             model = list(models.values())[i]
 
-            model.fit(X_train, y_train)  # Train model
+            param = params[list(models.keys())[i]]
+
+            gv = GridSearchCV(model, param, cv=5)
+            gv.fit(X_train,y_train)
+
+            model.set_params(**gv.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
